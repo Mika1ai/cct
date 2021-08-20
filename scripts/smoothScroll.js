61b69d8adf1fd4056ffe742108efbd374274e236
Mikalai <baranau.mikalai@gmail.com>
2021-08-20 12:59:23 +0300

    without cursor

--- scripts/smoothScroll.js
+++
@@ -1,9 +0,0 @@
-SmoothScroll({
-  animationTime: 1000, // [ms]
-  stepSize: 100, // [px]
-  accelerationDelta: 20,
-  accelerationMax: 5,
-  keyboardSupport: true,
-  arrowScroll: 50, // [px]
-  touchpadSupport: true,
-});
