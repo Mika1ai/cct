bd9fa80233e080865b27c99f73ba6f084aea0036
Mika1ai <baranau.mikalai@gmail.com>
2021-08-31 15:37:18 +0300

    cursor test v1

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,10 +1,13 @@
 "use strict";
+
 if (window.innerWidth > 1050) {
-  let cursorType = "default";
+	const cursorBlock = document.querySelector(".cursor-block");
+
 
   const cursor = document.querySelector(".cursor");
   const cursorImage = document.querySelector(".cursor__image");
-  const cursorElements = document.querySelectorAll(".cc-block");
+	const cursorElements = document.querySelectorAll(".cc-block");
+	let cursorType = "default";
   let x = 0;
   let y = 0;
 
@@ -20,18 +23,16 @@ if (window.innerWidth > 1050) {
 
       if (cursorType !== "swipe") {
         cursor.classList.remove("hidden");
-        document.body.style.cursor = "none";
       }
     });
     cursorElements[i].addEventListener("mouseleave", () => {
       cursor.classList.add("hidden");
-      document.body.style.cursor = "auto";
     });
   }
 
   document.addEventListener("mousemove", function (e) {
-    cursor.style.left = `${e.clientX - 37.5}px`;
-    cursor.style.top = `${e.clientY - 37.5}px`;
+    // cursor.style.left = `${e.clientX - 37.5}px`;
+    // cursor.style.top = `${e.clientY - 37.5}px`;
     x = e.clientX;
     y = e.clientY;
   });
@@ -40,10 +41,10 @@ if (window.innerWidth > 1050) {
 		if(document.elementFromPoint(x, y) !== null) {
 			if (document.elementFromPoint(x, y).closest(".cc-block")) {
 				cursor.classList.remove("hidden");
-				document.body.style.cursor = "none";
+				// document.body.style.cursor = "none";
 			} else {
 				cursor.classList.add("hidden");
-				document.body.style.cursor = "auto";
+				// document.body.style.cursor = "auto";
 			}
 		}
   });
@@ -53,7 +54,7 @@ if (window.innerWidth > 1050) {
   testimonialsSwiper.on("touchStart", () => {
     cursor.classList.add("hidden");
     cursorDrag.classList.remove("hidden");
-    document.body.style.cursor = "none";
+    // document.body.style.cursor = "none";
     cursorType = "swipe";
   });
 
@@ -63,16 +64,32 @@ if (window.innerWidth > 1050) {
     cursorType = "default";
   });
 
-  (function () {
-    gsap.set(cursorDrag, {
+  // (function () {
+  //   gsap.set(cursorDrag, {
+  //     xPercent: -50,
+  //     yPercent: -50,
+  //     scale: 1,
+  //   });
+  //   document.addEventListener("pointermove", movecursor);
+  //   function movecursor(e) {
+  //     gsap.to(cursorDrag, {
+  //       duration: 0.000001,
+  //       x: e.clientX,
+  //       y: e.clientY,
+  //     });
+  //   }
+	// })();
+	
+	(function () {
+    gsap.set(cursorBlock, {
       xPercent: -50,
       yPercent: -50,
       scale: 1,
     });
     document.addEventListener("pointermove", movecursor);
     function movecursor(e) {
-      gsap.to(cursorDrag, {
-        duration: 0.01,
+      gsap.to(cursorBlock, {
+        duration: 0.000001,
         x: e.clientX,
         y: e.clientY,
       });
