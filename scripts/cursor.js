c232454426716019eb2f3a2dcfe5df2967ab7d2f
Mika1ai <baranau.mikalai@gmail.com>
2021-09-01 09:55:06 +0300

    cursor test v2

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -2,9 +2,8 @@
 
 if (window.innerWidth > 1050) {
 	const cursorBlock = document.querySelector(".cursor-block");
-
-
-  const cursor = document.querySelector(".cursor");
+	const cursor = document.querySelector(".cursor");
+	const cursorDrag = document.querySelector(".cursor-drag");
   const cursorImage = document.querySelector(".cursor__image");
 	const cursorElements = document.querySelectorAll(".cc-block");
 	let cursorType = "default";
@@ -12,87 +11,43 @@ if (window.innerWidth > 1050) {
   let y = 0;
 
   for (let i = 0; i < cursorElements.length; i++) {
-    cursorElements[i].addEventListener("mouseenter", () => {
-      if (cursorElements[i].getAttribute("data-cursor") === "google") {
-        cursorImage.setAttribute("src", "./images/cursor-google.svg");
-      } else if (cursorElements[i].getAttribute("data-cursor") === "facebook") {
-        cursorImage.setAttribute("src", "./images/cursor-facebook.svg");
-      } else {
-        cursorImage.setAttribute("src", "./images/arrow.svg");
-      }
+		cursorElements[i].addEventListener("mouseenter", () => {
+			if (cursorElements[i].getAttribute("data-cursor") === "google") {
+				cursorImage.setAttribute("src", "./images/cursor-google.svg");
+			} else if (cursorElements[i].getAttribute("data-cursor") === "facebook") {
+				cursorImage.setAttribute("src", "./images/cursor-facebook.svg");
+			} else {
+				cursorImage.setAttribute("src", "./images/arrow.svg");
+			}
+		});
+	}
+	
+	document.addEventListener("pointermove", function (e) {
+		cursorBlock.style.left = `${e.clientX - 37.5}px`;
+		cursorBlock.style.top = `${e.clientY - 37.5}px`;
+		x = e.clientX;
+		y = e.clientY;
 
-      if (cursorType !== "swipe") {
-        cursor.classList.remove("hidden");
-      }
-    });
-    cursorElements[i].addEventListener("mouseleave", () => {
-      cursor.classList.add("hidden");
-    });
-  }
-
-  document.addEventListener("mousemove", function (e) {
-    // cursor.style.left = `${e.clientX - 37.5}px`;
-    // cursor.style.top = `${e.clientY - 37.5}px`;
-    x = e.clientX;
-    y = e.clientY;
-  });
-
-	document.addEventListener("scroll", () => {
 		if(document.elementFromPoint(x, y) !== null) {
 			if (document.elementFromPoint(x, y).closest(".cc-block")) {
-				cursor.classList.remove("hidden");
-				// document.body.style.cursor = "none";
+				if (cursorType !== "swipe") {
+					cursor.classList.remove("hidden");
+					cursorDrag.classList.add("hidden");
+				} else {
+					cursorDrag.classList.remove("hidden");
+					cursor.classList.add("hidden");
+				}
 			} else {
 				cursor.classList.add("hidden");
-				// document.body.style.cursor = "auto";
+				cursorDrag.classList.add("hidden");
 			}
-		}
-  });
-
-  const cursorDrag = document.querySelector(".cursor-drag");
+		}	
+	});
 
   testimonialsSwiper.on("touchStart", () => {
-    cursor.classList.add("hidden");
-    cursorDrag.classList.remove("hidden");
-    // document.body.style.cursor = "none";
     cursorType = "swipe";
   });
-
   testimonialsSwiper.on("touchEnd", () => {
-    cursor.classList.remove("hidden");
-    cursorDrag.classList.add("hidden");
     cursorType = "default";
   });
-
-  // (function () {
-  //   gsap.set(cursorDrag, {
-  //     xPercent: -50,
-  //     yPercent: -50,
-  //     scale: 1,
-  //   });
-  //   document.addEventListener("pointermove", movecursor);
-  //   function movecursor(e) {
-  //     gsap.to(cursorDrag, {
-  //       duration: 0.000001,
-  //       x: e.clientX,
-  //       y: e.clientY,
-  //     });
-  //   }
-	// })();
-	
-	(function () {
-    gsap.set(cursorBlock, {
-      xPercent: -50,
-      yPercent: -50,
-      scale: 1,
-    });
-    document.addEventListener("pointermove", movecursor);
-    function movecursor(e) {
-      gsap.to(cursorBlock, {
-        duration: 0.000001,
-        x: e.clientX,
-        y: e.clientY,
-      });
-    }
-  })();
 }
