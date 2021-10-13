75381b7808e000177a5f7506d072de939dd84ff4
Kirill Sakun <kirillsakun03@gmail.com>
2021-10-13 16:56:33 +0300

    cursor optimizing, small refactoring

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,74 +1,102 @@
 "use strict";
 
+
 if (window.innerWidth > 1050) {
-	const cursorBlock = document.querySelector(".cursor-block");
-	const cursor = document.querySelector(".cursor");
-	const cursorDrag = document.querySelector(".cursor-drag");
+  const cursorBlock = document.querySelector(".cursor-block");
+  const cursor = document.querySelector(".cursor");
+  const cursorDrag = document.querySelector(".cursor-drag");
   const cursorImage = document.querySelector(".cursor__image");
-	const cursorElements = document.querySelectorAll(".cc-block");
-	let cursorType = "default";
+  const cursorElements = document.querySelectorAll(".cc-block");
+  const cursorSize = 75;
+  let cursorType = "default";
   let x = 0;
   let y = 0;
 
   for (let i = 0; i < cursorElements.length; i++) {
-		cursorElements[i].addEventListener("mouseenter", () => {
-			if (cursorElements[i].getAttribute("data-cursor") === "google") {
-				cursorImage.setAttribute("src", "./images/cursor-google.svg");
-			} else if (cursorElements[i].getAttribute("data-cursor") === "facebook") {
-				cursorImage.setAttribute("src", "./images/cursor-facebook.svg");
-			} else {
-				cursorImage.setAttribute("src", "./images/arrow.svg");
-			}
-		});
-	}
-	
-	document.addEventListener("pointermove", function (e) {
-		cursorBlock.style.left = `${e.clientX - 37.5}px`;
-		cursorBlock.style.top = `${e.clientY - 37.5}px`;
-		x = e.clientX;
-		y = e.clientY;
+    cursorElements[i].addEventListener("mouseenter", () => {
+      if (cursorElements[i].getAttribute("data-cursor") === "google") {
+        cursorImage.setAttribute("src", "./images/cursor-google.svg");
+      } else if (cursorElements[i].getAttribute("data-cursor") === "facebook") {
+        cursorImage.setAttribute("src", "./images/cursor-facebook.svg");
+      } else {
+        cursorImage.setAttribute("src", "./images/arrow.svg");
+      }
+    });
+  }
 
-		if(document.elementFromPoint(x, y) !== null) {
-			if (document.elementFromPoint(x, y).closest(".cc-block")) {
-				if (cursorType !== "swipe") {
-					cursor.classList.remove("hidden");
-					cursorDrag.classList.add("hidden");
-				} else {
-					cursorDrag.classList.remove("hidden");
-					cursor.classList.add("hidden");
-				}
-			} else {
-				cursor.classList.add("hidden");
-				cursorDrag.classList.add("hidden");
-			}
-		}	
-	});
 
-	document.addEventListener("scroll", () => {
-		if(document.elementFromPoint(x, y) !== null) {
-			if (document.elementFromPoint(x, y).closest(".cc-block")) {
-				cursor.classList.remove("hidden");
-			} else {
-				cursor.classList.add("hidden");
-			}
-		}	
-	});
+  function moveCursor(e) {
+    x = e.clientX;
+    y = e.clientY;
+    cursorBlock.style.left = `${x - cursorSize / 2}px`;
+    cursorBlock.style.top = `${y - cursorSize / 2}px`;
 
-	function swiperTouch(swiper) {
-		swiper.on("touchStart", () => {
-			cursorType = "swipe";
-		});
-		swiper.on("touchEnd", () => {
-			cursorType = "default";
-		});
-	}
+    const hoveredEl = e.target
 
-	
-	if (typeof testimonialsSwiper !== "undefined") {
-		swiperTouch(testimonialsSwiper);
-	} else if (typeof ppSwiper !== "undefined") {
-		swiperTouch(ppSwiper);
-	} else if (typeof innerSwiper !== "undefined") {
-		swiperTouch(innerSwiper);
-	}
+    if (hoveredEl !== null) {
+      if (hoveredEl.closest(".cc-block")) {
+        if (cursorType !== "swipe") {
+          cursor.classList.remove("hidden");
+          cursorDrag.classList.add("hidden");
+        } else {
+          cursorDrag.classList.remove("hidden");
+          cursor.classList.add("hidden");
+        }
+      } else {
+        cursor.classList.add("hidden");
+        cursorDrag.classList.add("hidden");
+      }
+    }
+  }
+
+  function limitExecByInterval(fn, time) {
+    let lock, execOnUnlock, args;
+    return function () {
+      args = arguments;
+      if (!lock) {
+        lock = true;
+        setTimeout(function () {
+          lock = false;
+          if (execOnUnlock) {
+            execOnUnlock = false;
+          }
+        }, time);
+        return fn.apply(this, args);
+      } else execOnUnlock = true;
+    }
+  }
+
+  const limitedMoveCursor = limitExecByInterval(moveCursor, 1000 / 60)
+  document.addEventListener("pointermove", (e) => {
+    limitedMoveCursor(e);
+    // moveCursor(e); if you wanna compare with how it was before just uncomment this line and comment previous
+  });
+
+  document.addEventListener("scroll", () => {
+    if (document.elementFromPoint(x, y) !== null) {
+      if (document.elementFromPoint(x, y).closest(".cc-block")) {
+        cursor.classList.remove("hidden");
+      } else {
+        cursor.classList.add("hidden");
+      }
+    }
+  });
+
+  function swiperTouch(swiper) {
+    swiper.on("touchStart", () => {
+      cursorType = "swipe";
+    });
+    swiper.on("touchEnd", () => {
+      cursorType = "default";
+    });
+  }
+
+
+  if (typeof testimonialsSwiper !== "undefined") {
+    swiperTouch(testimonialsSwiper);
+  } else if (typeof ppSwiper !== "undefined") {
+    swiperTouch(ppSwiper);
+  } else if (typeof innerSwiper !== "undefined") {
+    swiperTouch(innerSwiper);
+  }
 }
